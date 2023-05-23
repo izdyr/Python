25af975a474349dfdfbb9c34d118798421720eda
Izadyar Jalalipour <102060761+izadyarjalalipour@users.noreply.github.com>
2023-05-23 22:21:11 +0330

    passwordgenerator.py

---
+++ Password Generator/passwordgenerator.py
@@ -0,0 +1,10 @@
+import random
+# Get number of characters
+number = int(input("Enter Enter the number of characters: "))
+old_password = input("Enter your old password: ")
+# Useful characters in password 
+character="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+# Making password
+password =  "".join(random.sample(character, old_password, number ))
+# Print password
+print (password)
