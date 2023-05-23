4cf35d416cc2345c588a0c1009422cefbbe79a74
Izadyar Jalalipour <102060761+izadyarjalalipour@users.noreply.github.com>
2023-05-23 20:21:42 +0330

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,21 @@
-print("Hello")
+# Print the tutorial and get the problem to solve it.
+print("This is Calculator! You can make your question with this symbols (+,  -, x or *, / or ÷)")
+question = input("What do you want to know? ")
+# Definition of functions in the order of question, solution, answer.
+x, y, z = question.split()
+x, z = float(x), float(z)
+
+# How to solve question ?
+if y == "+":
+    answer = x + z
+elif y == "-":
+    answer = x - z
+elif y == "*" or "x":
+    answer = x * z
+elif y == "/" or "÷" and z != 0:
+    answer = x / z
+# Print Eror if there is no way to solve it
+else:
+    print("Eror")
+# print answer
+print(question, "=", answer)
