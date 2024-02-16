5f13f9cd1bc986fe1cf8f999a59441da37f9b21d
Izadyar Jalalipour <102060761+izadyarjalalipour@users.noreply.github.com>
2024-02-16 20:12:13 +0330

    Update qrcode_generator.py

--- QR code generator/qrcode_generator.py
+++ QR code generator/qrcode_generator.py
@@ -4,6 +4,9 @@ import qrcode
 # Get data from website URL
 data = input("Enter URL: ")
 
+# Get a name for png
+picture_name = input("Enter a name for qrcode: ")
+
 # Qr code effect
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 
@@ -14,4 +17,4 @@ qr.make(fit=True)
 img = qr.make_image(fill="black", back_color="white")
 
 # Download qrcode image
-img.save("qrcode.png")
+img.save(picture_qrcode)
